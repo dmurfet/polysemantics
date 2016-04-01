79987734b667d37d96d41a3d26c7e3db40b9b882
Daniel Murfet <daniel.murfet@gmail.com>
2016-04-01 22:13:40 +1100

    New code.

--- hidden.py
+++ hidden.py
@@ -147,16 +147,26 @@ def main(argv=None):
     	
     	# Iterate and train.
     	for step in xrange(num_epochs * train_size // BATCH_SIZE):
-    	    if verbose:
-    	        print step,
-    	        
     	    offset = (step * BATCH_SIZE) % train_size
     	    batch_data = train_data[offset:(offset + BATCH_SIZE), :]
-    	    batch_labels = train_labels[offset:(offset + BATCH_SIZE)]
-    	    train_step.run(feed_dict={x: batch_data, y_: batch_labels})
-    	    if verbose and offset >= train_size-BATCH_SIZE:
-    	        print
-    	print "Accuracy:", accuracy.eval(feed_dict={x: test_data, y_: test_labels})
+            batch_labels = train_labels[offset:(offset + BATCH_SIZE)]
+            feed={x: batch_data, y_: batch_labels}
+    	        
+            result=sess.run([merged,accuracy],feed_dict=feed)
+            summary_str = result[0]
+            acc = result[1]
+            writer.add_summary(summary_str, step)
+            print("Accuracy at step %s: %s" % (step, acc))
+    	    
+    	    # new end
+    	    
+    	    #old    
+    	    #offset = (step * BATCH_SIZE) % train_size
+    	    #batch_data = train_data[offset:(offset + BATCH_SIZE), :]
+    	    #batch_labels = train_labels[offset:(offset + BATCH_SIZE)]
+    	    #train_step.run(feed_dict={x: batch_data, y_: batch_labels})
+    	    
+    	    #print "Accuracy:", accuracy.eval(feed_dict={x: test_data, y_: test_labels})
             
 if __name__ == '__main__':
     tf.app.run()
